5fc3c317945301212a8f245bc2e6ffb22c91705c
박상규 <park78951@gmail.com>
2021-06-03 14:09:16 +0900

    test: Todo App 레이아웃 테스트 코드 작성

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,4 +2,9 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("", () => {});
+describe("TODO 앱", () => {
+  it("TODO 리스트 앱이 화면에 출력된다", () => {
+    const { getByText } = render(<App />);
+    expect(getByText("TODO 리스트")).toBeInTheDocument();
+  });
+});
